4ede2a98abe1c763e627bd4310e7e9e58aedd8b6
Starboy's MacBook <pcworld@PCs-MacBook-Pro.local>
2021-01-25 03:13:57 +0600

    Updates

---
+++ precache-manifest.84165d15605d27e0b8fc4038d3a6db2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c3c4a7d1955b32912a9188bed02fa3",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "715714e6f168e99defef",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "91cfe4284a4f76d2cbe3",
+    "url": "/home/static/js/2.91378140.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.91378140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715714e6f168e99defef",
+    "url": "/home/static/js/main.27ee66ef.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "e30d2367bb21e08b9a6d4f40a4f02d73",
+    "url": "/home/static/media/borson.e30d2367.jpeg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "37b3a2f65bae72b40229d85656a8687f",
+    "url": "/home/static/media/resume.37b3a2f6.pdf"
+  }
+]);
